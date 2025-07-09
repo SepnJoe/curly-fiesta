041b2e1dd602760d09081d4ab0cc0dc8acea16c8
LRCSNIF <niklas.feldinger@r-software.at>
2025-07-09 14:26:02 +0200

    HTML CSS PHP Projekt

--- php/HTML/project/sql/db.sql
+++ php/HTML/project/sql/db.sql
@@ -9,43 +9,42 @@ CREATE TABLE IF NOT EXISTS Benutzer (
                                         Passwort VARCHAR(255) NOT NULL
 );
 
--- Tabelle: Anschrift
-CREATE TABLE IF NOT EXISTS Anschrift (
-                                        ID INT AUTO_INCREMENT PRIMARY KEY,
-                                        Strasse VARCHAR(255) NOT NULL,
-                                        Hausnummer VARCHAR(50) NOT NULL,
-                                        ort_id INT NOT NULL,
-                                        FOREIGN KEY (ort_id) REFERENCES Ort(ID)
-);
-
--- Tabelle: Ort
-CREATE TABLE IF NOT EXISTS Ort (
-                                        ID INT AUTO_INCREMENT PRIMARY KEY,
-                                        plz VARCHAR(20) NOT NULL,
-                                        Ortsname VARCHAR(255) NOT NULL,
-                                        land_id INT NOT NULL,
-                                        FOREIGN KEY (land_id) REFERENCES Land(ID)
-);
-
 -- Tabelle: Land
 CREATE TABLE IF NOT EXISTS Land (
                                         ID INT AUTO_INCREMENT PRIMARY KEY,
                                         Landname VARCHAR(255) NOT NULL
 );
 
+-- Tabelle: Ort
+CREATE TABLE IF NOT EXISTS Ort (
+                                   ID INT AUTO_INCREMENT PRIMARY KEY,
+                                   plz VARCHAR(20) NOT NULL,
+                                   Ortsname VARCHAR(255) NOT NULL,
+                                   land_id INT NOT NULL,
+                                   FOREIGN KEY (land_id) REFERENCES Land(ID)
+);
+
+-- Tabelle: Anschrift
+CREATE TABLE IF NOT EXISTS Anschrift (
+                                         ID INT AUTO_INCREMENT PRIMARY KEY,
+                                         Strasse VARCHAR(255) NOT NULL,
+                                         Hausnummer VARCHAR(50) NOT NULL,
+                                         ort_id INT NOT NULL,
+                                         FOREIGN KEY (ort_id) REFERENCES Ort(ID)
+);
+
+
 -- Tabelle: Person
 CREATE TABLE IF NOT EXISTS Person (
                                       ID INT AUTO_INCREMENT PRIMARY KEY,
-                                      BenutzerID INT,
+                                      BenutzerID INT NOT NULL ,
                                       Anrede VARCHAR(10),
-                                      Vorname VARCHAR(100),
-                                      Nachname VARCHAR(100),
-                                      Geburtstag DATE,
-                                      Strasse VARCHAR(255),
-                                      Wohnort VARCHAR(100),
-                                      PLZ VARCHAR(10),
+                                      Vorname VARCHAR(100) NOT NULL ,
+                                      Nachname VARCHAR(100) NOT NULL ,
+                                      Geburtstag DATE NOT NULL ,
                                       Telefonnummer VARCHAR(20),
-                                      Bundesland VARCHAR(100),
                                       ProfilBild VARCHAR(255),
-                                      FOREIGN KEY (BenutzerID) REFERENCES Benutzer(ID) ON DELETE CASCADE
+                                      Anschrift_id INT NOT NULL,
+                                      FOREIGN KEY (BenutzerID) REFERENCES Benutzer(ID),
+                                      FOREIGN KEY (Anschrift_id) REFERENCES Anschrift(ID)
 );
